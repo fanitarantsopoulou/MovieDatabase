d310b1725a7f533a7f4d23252038a4687d4de938
Fani Tarantsopoulou <fanitarants@gmail.com>
2023-11-01 14:44:15 +0200

    Update ergasia1.c

--- ergasia1.c
+++ ergasia1.c
@@ -2,21 +2,23 @@
 struct movie
 {
     int code;
-    char title[26];
+    char title[25];
     int year;
 };
-#define movies 100
-void insert(int code, char title, int year)
+
+#define NAX_MOVIES 100;
+struct movie movies[MAX_MOVIES];
+int nmMovies=0;
+
+void insert()
 {
     // Inserting a movie at the database of the store
-    struct movie NewMovie;
-    scanf("%d", &NewMovie.code);
-    for (int i = 0; i < movies; i++) {
-    if (NewMovie[i].code == newMovie.code) {
-        printf("The movie already exists in the database.\n");
-        return;
+    struct movie newMovie;
+    for (int i=0;i<=max_movies;i++){
+        if (newMovie.code==.code){
+            printf("The movie doesn't exist in the database.\n");
+        }
     }
-}
 }
 
 void search(int code)
@@ -32,9 +34,11 @@ void update(int code)
 void erase(int code)
 {
     // Erasing the movie by entering its code
+    printf("Enter the movie code\n");
+
 }
 
-void print(int code, char title, int year)
+void print(int code, char *title[25], int *year)
 {
     // Printing all the information that are located at the store's database
 }
@@ -42,58 +46,76 @@ void print(int code, char title, int year)
 void quit()
 {
     // Quitting the program
+
 }
 
 int main(int argc, char *argv[])
 {
-    printf("Welcome to movies adminstration system");
+    printf("Welcome to movies adminstration system\n");
+    printf("\ni for insert\n");
+    printf("\ns for search\n");
+    printf("\nu for update\n");
+    printf("\ne for erase\n");
+    printf("\np for print\n");
+    printf("\nq for quit\n");
 
     char choice;
     struct movie newMovie;
-
+    printf("\nEnter your choice:");
     // The user is asked to select a function
-    scanf("Enter your choice %c:", &choice);
+    scanf("%C", &choice);
     // All the letters at the case command, are the first letters of each function.
-    switch (choice){
+    switch (choice)
+    {
     case 'i':
     case 'I':
-        printf("Enter movie code: %d", newMovie.code);
-        printf("Enter movie title: %c", newMovie.title);
-        printf("Enter year of movie: %d", newMovie.year);
+        printf("Enter movie code: ");
+        scanf("%d", newMovie.code);
+        printf("Enter movie title: ");
+        scanf("%s", newMovie.title);
+        printf("Enter year of movie: ");
+        scanf("%d", &newMovie.year);
         insert(newMovie.code, newMovie.title, newMovie.year);
         break;
 
     case 's':
     case 'S':
-        printf("Enter movie code: %d", newMovie.code);
+        printf("Enter movie code: ");
+        scanf("%d", newMovie.code);
+        search(newMovie.code);
         break;
 
     case 'u':
     case 'U':
-        printf("Enter movie code: %d", newMovie.code);
+        printf("Enter movie code: ");
+        scanf("%d", newMovie.code);
+        update(newMovie.code);
 
         break;
 
     case 'e':
     case 'E':
-        printf("Enter movie code: %d", newMovie.code);
+        printf("Enter movie code: ");
+        scanf("%d", newMovie.code);
+        erase(newMovie.code);
 
         break;
 
     case 'p':
     case 'P':
-        printf("Enter movie code: %d", newMovie.code);
-        printf("Enter movie title: %c", newMovie.title);
-        printf("Enter year of movie: %d ", newMovie.year);
+        printf("Enter movie code: ");
+        scanf("%d", newMovie.code);
+        printf("Enter movie title: ");
+        scanf("%s", newMovie.title);
+        printf("Enter year of movie: ");
+        scanf("%d", &newMovie.year);
+        print(newMovie.code, newMovie.title, newMovie.year);
         break;
 
     case 'q':
     case 'Q':
-        printf("Enter movie code: %d", newMovie.code);
-        printf("Enter movie title: %c", newMovie.title);
-        printf("Enter year of movie: %d ", newMovie.year);
+        quit();
         break;
-
     default:
         printf("The function you have selected doesn't exist.");
         // default statements
